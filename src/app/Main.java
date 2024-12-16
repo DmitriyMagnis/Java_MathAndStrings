d67a279a0d785be6753f9b00a51a690c8848cfcd
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-12-16 12:05:55 +0100

    ADD/count Upper letters in words

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
         MathOperation math = new MathOperation() {
@@ -11,9 +13,11 @@ public class Main {
 
         StringManipulator stringManipulator = String::toUpperCase;
 
+        Function<String, Integer> upperCaseCount = StringListProcessor::countUppercase;
 
-        System.out.println("StringManipulator: " + stringManipulator.toUpperCase("string"));
         System.out.println("MathOperation: " + math.operate(2,3));
+        System.out.println("toUpperCase: " + stringManipulator.toUpperCase("string"));
+        System.out.println("countUppercase: " + upperCaseCount.apply("StR"));
 
     }
 }
---
+++ src/app/StringListProcessor.java
@@ -0,0 +1,18 @@
+package app;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class StringListProcessor {
+    static int countUppercase(String s) {
+        if (s == null || s.isEmpty()) return 0;
+
+        AtomicInteger count = new AtomicInteger(0);
+
+        for (char c: s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                count.incrementAndGet();
+            }
+        }
+        return count.get();
+    }
+}
